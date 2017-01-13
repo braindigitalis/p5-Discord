b44f6c45d2cf6f61194e440e81c4d9f0d598ece0
Brad Haywood <brad.haywood@opusvl.com>
2017-01-13 08:40:02 +0000

    changed example

--- lib/Discord.pm
+++ lib/Discord.pm
@@ -101,9 +101,9 @@ Version 0.001
   use Discord;
   
   my $discord = Discord->new(
-      client_id => '259521088327909380',
-      client_secret => 'Z9FFr8u4gBwxnFw-RDjeqSAngAq2CglT',
-      token => 'MjU5NTIxMDg4MzI3OTA5Mzgw.C1j_4w.u63P40UIx90TguO3935mVSya13Y',
+      client_id => 'SOME_ID',
+      client_secret => 'YOUR_SECRET',
+      token => 'TOKEN_FOR_BOT',
       bot => 1,
   );
   
