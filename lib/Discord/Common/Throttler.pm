b29801e3fca1ca5bff86ed5d9920c6fc0c44f2fd
Brad Haywood <brad.haywood@opusvl.com>
2017-01-18 18:39:13 +0000

    Updated throttle work

--- lib/Discord/Common/Throttler.pm
+++ lib/Discord/Common/Throttler.pm
@@ -1,28 +1,39 @@
 package Discord::Common::Throttler;
 
 use Discord::Loader;
+use Time::Piece;
 
 has 'frequency' => ( is => 'rw' );
-has 'limit'		=> ( is => 'rw' );
+has 'limit'     => ( is => 'rw' );
+has 'callers'   => ( is => 'rw', default => sub { {} } );
 
-sub apply_to {
-	my ($self, $func) = @_;
-	my $throttle = sub {
-		warn "Throttling $func";
-	};
+# callers = {
+#    'Package::function' => {
+#        'last_run' => TIMESTAMP,
+#    },
+#}
 
-	throttle: {
-		no strict 'refs';
-		no warnings 'redefine';
 
-		my $orig = \&{$func};
+method apply ($cb) {
+    my @caller = (caller(1));
+    my $path   = $caller[3]; # Package::function
 
-		*{"${func}"} = sub {
-			warn $throttle;
-            $throttle->(@_);
-            $orig->(@_);
+    # girl, we got history
+    if (exists $self->callers->{$path}) {
+        # get the last time it was ran
+        my $last_run = $self->callers->{$path}->{last_run};
+        my $hits     = $self->callers->{$path}->{hits};
+    }
+    else {
+        $self->callers->{$path} = {
+            last_run => localtime,
+            hits     => 1,
         };
-	}
+    }
+
+    # finally issue the original callback if we haven't hit
+    # the rate limit
+    $cb->(@_);
 }
 
 1;
