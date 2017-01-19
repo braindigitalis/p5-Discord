f0e3909cc1c3a31072a8c1274d21758994a7ea29
Brad Haywood <brad.haywood@opusvl.com>
2017-01-19 11:51:07 +0000

    Fixed up error code checking

--- lib/Discord/Client/WebSocket/Events/Errors.pm
+++ lib/Discord/Client/WebSocket/Events/Errors.pm
@@ -3,44 +3,46 @@ package Discord::Client::WebSocket::Events::Errors;
 use Discord::Loader as => 'Role';
 
 method handle_error ($code) {
-    if(Discord::Constants::OPCodes::INVALID_SESSION){
-      return "Invalid Session";
-    }
-    elsif(Discord::Constants::CloseCodes::UNKNOWN_ERROR){
-      return "Unknown Error";
-    }
-    elsif(Discord::Constants::CloseCodes::UNKNOWN_OPCODE){
-      return "Invalid Gateway Opcode";
-    }
-    elsif(Discord::Constants::CloseCodes::DECODE_ERROR){
-      return "Invalid Payload";
-    }
-    elsif(Discord::Constants::CloseCodes::NOT_AUTH){
-      return "Not Authenticated";
-    }
-    elsif(Discord::Constants::CloseCodes::AUTH_FAILED){
-      return "Invalid Token";
-    }
-    elsif(Discord::Constants::CloseCodes::ALREADY_AUTH){
-      return "Already Authenticated";
-    }
-    elsif(Discord::Constants::CloseCodes::INVALID_SEQ){
-      return "Invalid sequence when resuming";
-    }
-    elsif(Discord::Constants::CloseCodes::RATE_LIMITED){
-      return "You are being rate limited";
-    }
-    elsif(Discord::Constants::CloseCodes::SESSION_TIMEOUT){
-      return "Session timed out";
-    }
-    elsif(Discord::Constants::CloseCodes::INVALID_SHARD){
-      return "Invalid Shard";
-    }
-    elsif(Discord::Constants::CloseCodes::SHARDING_REQUIRED){
-      return "Exceeded guild limit, sharding required";
-    }
-    else {
-      return "Unknown Error"
+    for ($code) {
+        if($_ == Discord::Constants::OPCodes::INVALID_SESSION){
+          return "Invalid Session";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::UNKNOWN_ERROR){
+          return "Unknown Error";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::UNKNOWN_OPCODE){
+          return "Invalid Gateway Opcode";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::DECODE_ERROR){
+          return "Invalid Payload";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::NOT_AUTH){
+          return "Not Authenticated";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::AUTH_FAILED){
+          return "Invalid Token";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::ALREADY_AUTH){
+          return "Already Authenticated";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::INVALID_SEQ){
+          return "Invalid sequence when resuming";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::RATE_LIMITED){
+          return "You are being rate limited";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::SESSION_TIMEOUT){
+          return "Session timed out";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::INVALID_SHARD){
+          return "Invalid Shard";
+        }
+        elsif($_ == Discord::Constants::CloseCodes::SHARDING_REQUIRED){
+          return "Exceeded guild limit, sharding required";
+        }
+        else {
+          return "Unknown Error"
+        }
     }
 }
 
