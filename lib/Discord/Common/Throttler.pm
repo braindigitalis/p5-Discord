564a38f776ef6aa1eac274d96058348ee6bb6814
Brad Haywood <brad@perlpowered.com>
2017-01-21 10:13:55 +0000

    Throttler is now asynchronous and polls every second. It's a start...

--- lib/Discord/Common/Throttler.pm
+++ lib/Discord/Common/Throttler.pm
@@ -1,33 +1,50 @@
 package Discord::Common::Throttler;
 
 use Discord::Loader;
+use Mojo::IOLoop;
 
 has 'frequency' => ( is => 'rw' );
 has 'limit'     => ( is => 'rw' );
 has 'last_hit'  => ( is => 'rw', default => sub { time } );
 has 'queue'     => ( is => 'rw', default => sub { {} } );
 
+method BUILD {
+    my @sorted;
+    Mojo::IOLoop->recurring(1, sub {
+        if (keys %{$self->queue}) {
+            @sorted = sort { $a <=> $b } keys %{$self->queue};
+            my $next_key = $sorted[0];
+            my $next = delete $self->queue->{$next_key};
+            $next->(@_);
+        }
+    });
+}
+
 method apply ($cb) {
     my @caller = (caller(1));
     my $path   = $caller[3]; # Package::function
     my $sleep_for;
-
-    # if we're sending shit too fast add it to the queue
     my $time = time;
-    if (($time - $self->last_hit) < $self->frequency) {
-        warn "[Trottled]: Adding $path to queue (" . ($time - $self->last_hit) . ")"
-            if $ENV{DISCORD_DEBUG};
-        $sleep_for = ($self->frequency - ($time - $self->last_hit));
-        say "=> SLEEPING FOR $sleep_for"
-            if $ENV{DISCORD_DEBUG};
-        sleep $sleep_for;
-        $self->last_hit(time);
-        $cb->(@_);
-    }
-    else {
-        $self->last_hit(time);
-        $cb->(@_);
-    }
+    $time = $time + 1
+        if exists $self->queue->{time};
+
+    $self->queue->{$time} = $cb;
+    # if we're sending shit too fast add it to the queue
+    #my $time = time;
+    #if (($time - $self->last_hit) < $self->frequency) {
+    #    warn "[Trottled]: Adding $path to queue (" . ($time - $self->last_hit) . ")"
+    #        if $ENV{DISCORD_DEBUG};
+    #    $sleep_for = ($self->frequency - ($time - $self->last_hit));
+    #    say "=> SLEEPING FOR $sleep_for"
+    #        if $ENV{DISCORD_DEBUG};
+    #    sleep $sleep_for;
+    #    $self->last_hit(time);
+    #    $cb->(@_);
+    #}
+    #else {
+    #    $self->last_hit(time);
+    #    $cb->(@_);
+   # }
 }
 
 1;
