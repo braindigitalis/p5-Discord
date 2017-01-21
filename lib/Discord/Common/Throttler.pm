a75af6771315e50e2f398f6ebd905df4ca3bf0b2
Brad Haywood <brad@perlpowered.com>
2017-01-21 10:22:11 +0000

    Removed old shitty code

--- lib/Discord/Common/Throttler.pm
+++ lib/Discord/Common/Throttler.pm
@@ -29,22 +29,6 @@ method apply ($cb) {
         if exists $self->queue->{time};
 
     $self->queue->{$time} = $cb;
-    # if we're sending shit too fast add it to the queue
-    #my $time = time;
-    #if (($time - $self->last_hit) < $self->frequency) {
-    #    warn "[Trottled]: Adding $path to queue (" . ($time - $self->last_hit) . ")"
-    #        if $ENV{DISCORD_DEBUG};
-    #    $sleep_for = ($self->frequency - ($time - $self->last_hit));
-    #    say "=> SLEEPING FOR $sleep_for"
-    #        if $ENV{DISCORD_DEBUG};
-    #    sleep $sleep_for;
-    #    $self->last_hit(time);
-    #    $cb->(@_);
-    #}
-    #else {
-    #    $self->last_hit(time);
-    #    $cb->(@_);
-   # }
 }
 
 1;
