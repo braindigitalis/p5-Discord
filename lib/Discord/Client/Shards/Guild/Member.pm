5b95f933641f725ed955b3abc5c7cdad698273d0
Brad Haywood <brad@perlpowered.com>
2017-01-22 10:11:59 +0000

    Removed redundant comments

--- lib/Discord/Client/Shards/Guild/Member.pm
+++ lib/Discord/Client/Shards/Guild/Member.pm
@@ -2,14 +2,6 @@ package Discord::Client::Shards::Guild::Member;
 
 use Discord::Loader;
 
-#
-#   'user' => {
-#	   'username' => 'BaconBotDev',
-#	   'id' => '145579551433424896',
-#	   'discriminator' => '4478',
-#	   'avatar' => 'f07338e22137a6e959b889379d8f3f1b'
-#	},
-
 has 'id'		=> ( is => 'rw' );
 has 'roles'		=> ( is => 'rw', default => sub { [] } );
 has 'username'	=> ( is => 'rw' );
